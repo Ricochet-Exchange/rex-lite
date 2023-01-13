aa331d0d76f31dea9859976026ee56215da78dd1
nitegeist <nitegeist.dev@gmail.com>
2023-01-13 17:21:00 -0500

    show contract errors in alert

--- utils/transformError.ts
+++ utils/transformError.ts
@@ -1,12 +1,15 @@
 export const transformError = (e?: any) => {
 	if (e?.data?.error) {
 		return e.data.error;
-	}
-	if (e.code === 4001) {
+	} else if (e?.error?.code === 4001) {
 		return e?.message;
+	} else if (e?.code === -32603) {
+		return e?.data?.message.replace('execution reverted:', '');
+	} else if (e?.error?.code === -32603) {
+		return e.error?.data?.message.replace('execution reverted:', '');
+	} else if (e?.message.includes('Already streaming')) {
+		return 'You already have a stream open with these tokens. Please try another set of tokens';
+	} else {
+		return 'Operation failed. Refresh the page or try again later.';
 	}
-	if (e.code === -32603) {
-		return e.data?.message;
-	}
-	return 'Operation failed. Refresh the page or try again later.';
 };
