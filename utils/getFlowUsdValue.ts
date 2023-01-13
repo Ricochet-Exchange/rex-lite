b1d538ab5cd6b8970e56e4bc3c36c96b40aa37ce
nitegeist <nitegeist.dev@gmail.com>
2023-01-13 17:22:14 -0500

    move getFlowUsdValue into it's own file

---
+++ utils/getFlowUsdValue.ts
@@ -0,0 +1,26 @@
+import { InvestmentFlow } from 'constants/flowConfig';
+
+export const getFlowUSDValue = (
+	flow: InvestmentFlow,
+	queries: Map<
+		string,
+		{
+			flowKey: string;
+			flowsReceived: number;
+			flowsOwned: string;
+			totalFlows: number;
+			placeholder: string;
+			subsidyRate: { perso: number; total: number; endDate: string };
+			streamedSoFar?: number;
+			receivedSoFar?: number;
+		}
+	>,
+	coingeckoPrices: Map<string, number>,
+	toFixed: number = 0
+) => {
+	return (
+		coingeckoPrices.size > 0 && queries.size > 0
+			? parseFloat(queries.get(flow.flowKey)?.flowsOwned!) * coingeckoPrices.get(flow.tokenA)!
+			: 0
+	).toFixed(toFixed);
+};
