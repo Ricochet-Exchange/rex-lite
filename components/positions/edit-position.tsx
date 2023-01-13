d1c574551f0f5613f5b170c5e22464826e7fa76b
nitegeist <nitegeist.dev@gmail.com>
2023-01-12 21:04:37 -0500

    Add stop stream functionality

--- components/positions/edit-position.tsx
+++ components/positions/edit-position.tsx
@@ -1,6 +1,9 @@
+import AlertAction from '@richochet/utils/alertAction';
+import { AlertContext } from 'contexts/AlertContext';
 import { NextPage } from 'next';
 import { useTranslation } from 'next-i18next';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import streamApi from 'redux/slices/streams.slice';
 import { OutlineButton, RoundedButton } from '../button';
 import { PositionData } from './positions';
 
@@ -13,8 +16,31 @@ export const EditPosition: NextPage<Props> = ({ setClose, position }) => {
 	const { t } = useTranslation('home');
 	const [action, setAction] = useState('');
 	const [amount, setAmount] = useState('');
+	const [state, dispatch] = useContext(AlertContext);
+	const [stopStreamTrigger] = streamApi.useLazyStopStreamQuery();
 	const handleSubmit = (event: any) => {
 		event.preventDefault();
+	};
+	const handleStop = () => {
+		dispatch(AlertAction.showLoadingAlert('Waiting for your transaction to be confirmed...', ''));
+		const stopStream = stopStreamTrigger(position);
+		stopStream
+			.then((response) => {
+				if (response.isSuccess) {
+					dispatch(AlertAction.showSuccessAlert('Success', 'Transaction confirmed 👌'));
+				}
+				if (response.isError) {
+					console.log('response', response);
+					dispatch(AlertAction.showErrorAlert('Error', `${response?.error}`));
+				}
+				setTimeout(() => {
+					dispatch(AlertAction.hideAlert());
+				}, 5000);
+			})
+			.catch((error) => {
+				console.log({ error });
+				dispatch(AlertAction.showErrorAlert('Error', `${error || error?.message}`));
+			});
 	};
 	return (
 		<div className='flex flex-col space-y-4'>
@@ -23,7 +49,7 @@ export const EditPosition: NextPage<Props> = ({ setClose, position }) => {
 					<h6 className='text-slate-100'>{t('position-action')}?</h6>
 					<OutlineButton action={`${t('change-swap-amount')}`} type='button' handleClick={() => setAction('change')} />
 					<OutlineButton action={`${t('deposit')} ${position.coinA}`} type='button' />
-					<RoundedButton action={`${t('stop-position')}`} type='button' />
+					<RoundedButton action={`${t('stop-position')}`} type='button' handleClick={handleStop} />
 					<hr className='border-slate-500' />
 					<div className='flex justify-end'>
 						<button type='button' className='outline-none text-slate-100 underline' onClick={() => setClose(false)}>
