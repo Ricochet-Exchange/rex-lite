1a8a184c5e248a51ad3a0d7de544dbcccf0ac17e
nitegeist <nitegeist.dev@gmail.com>
2023-01-16 17:37:38 -0500

    Add custom link to uniswap

--- components/launchpad.tsx
+++ components/launchpad.tsx
@@ -1,9 +1,14 @@
 import { RectangleGroupIcon } from '@heroicons/react/24/solid';
+import { RICAddress, USDCAddress } from 'constants/polygon_config';
 import { useTranslation } from 'next-i18next';
 import { SolidButton } from './button';
 
 export const LaunchPad = () => {
 	const { t } = useTranslation('home');
+	const navigateToUniswap = () => {
+		const url = `https://app.uniswap.org/#/swap?theme=dark&inputCurrency=${USDCAddress}&outputCurrency=${RICAddress}&exactAmount=100000&exactField=output`;
+		window.open(url, '_blank');
+	};
 	return (
 		<div className='flex flex-col space-y-4'>
 			<div className='flex items-center justify-center text-slate-100 space-x-3'>
@@ -12,7 +17,7 @@ export const LaunchPad = () => {
 			</div>
 			<p className='text-slate-100'>{t('start-a-position')}</p>
 			<SolidButton type='button' primary={true} action={t('start-20-position')} />
-			<SolidButton type='button' primary={false} action={`${t('buy')} 100,000 RIC`} />
+			<SolidButton type='button' primary={false} action={`${t('buy')} 100,000 RIC`} handleClick={navigateToUniswap} />
 		</div>
 	);
 };
