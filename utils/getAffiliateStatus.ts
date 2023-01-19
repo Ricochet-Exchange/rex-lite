e3175841fe9cc73d1795cbc94cadd8e1fd6ba117
nitegeist <nitegeist.dev@gmail.com>
2023-01-18 22:12:22 -0500

    Add getAffiliateStatus function

---
+++ utils/getAffiliateStatus.ts
@@ -0,0 +1,56 @@
+import { readContract } from '@wagmi/core';
+import { referralABI } from 'constants/abis';
+import { rexReferralAddress } from 'constants/polygon_config';
+import { ethers } from 'ethers';
+
+export enum AFFILIATE_STATUS {
+	'INACTIVE' = 'inactive',
+	'REGISTERING' = 'registering',
+	'AWAITING_VERIFICATION' = 'awaitingVerification',
+	'ENABLED' = 'enabled',
+}
+
+const validations = (input: string): [boolean, string][] => {
+	const criteria: [boolean, string][] = [
+		[new Blob([input]).size <= 32, 'Cannot be larger than 32 characters (Some characters count as 2)'],
+		[input.length > 1, 'Must be at least 2 characters'],
+	];
+	return criteria;
+};
+
+export const filterValidationErrors = (input: string) =>
+	validations(input).reduce<string[]>((acc: string[], each) => {
+		if (each[0] === false) {
+			return [...acc, each[1]];
+		}
+		return acc;
+	}, []);
+
+export const getAffiliateStatus = async (address: string, setCurrentReferralId = (referralId: string) => {}) => {
+	const affiliateId: any = await readContract({
+		address: rexReferralAddress,
+		abi: referralABI,
+		functionName: 'addressToAffiliate',
+		args: [address.toLowerCase()],
+	});
+
+	const res: any = await readContract({
+		address: rexReferralAddress,
+		abi: referralABI,
+		functionName: 'affiliates',
+		args: [affiliateId],
+	});
+
+	if (ethers.BigNumber.from(res.addr).isZero()) {
+		return AFFILIATE_STATUS.INACTIVE;
+	}
+
+	if (res.enabled === false) {
+		return AFFILIATE_STATUS.AWAITING_VERIFICATION;
+	}
+
+	if (filterValidationErrors(res.id).length === 0) {
+		setCurrentReferralId(res.id);
+		return AFFILIATE_STATUS.ENABLED;
+	}
+};
