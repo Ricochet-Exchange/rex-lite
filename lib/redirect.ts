8df62cf0ba0260ad557090af9f04638fabd304dc
sky cherfa <sherifcherfa@gmail.com>
2023-03-17 07:21:39 -0400

    hack together a fix for the url redirect BS

--- lib/redirect.ts
+++ lib/redirect.ts
@@ -1,23 +1,37 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import languageDetector from './languageDetector';
 
 export const useRedirect = (to?: any) => {
 	const router = useRouter();
+	const [url, setUrl] = useState('');
 	to = to || router.asPath;
-
 	// language detection
+	//check if in localhost, if not just add the .html extension to the url when loading
 	useEffect(() => {
+		if (!url) {return}
 		const detectedLng = languageDetector.detect();
 		if (to.startsWith('/' + detectedLng) && router.route === '/404') {
 			// prevent endless loop
-			router.replace('/' + detectedLng + router.route);
+			if (url === 'localhost:3000') {
+				router.replace('/' + detectedLng + router.route);
+			} else {
+				router.replace('/' + detectedLng + '.html' )
+			}
 			return;
 		}
 
+		if (url === 'localhost:3000') {
+			router.replace('/' + detectedLng + to);
+		} else {
+			router.replace('/' + detectedLng + '.html' )
+		}
 		languageDetector.cache?.(detectedLng!);
-		router.replace('/' + detectedLng + to);
-	});
+	}, [url])
+
+	useEffect(() => {
+		setUrl(window ? window.location.host : '');
+	}, []);
 
 	return;
 };
