8fd94e0773a0b723e7c9ca199137b20c1c5d58e7
sky cherfa <sherifcherfa@gmail.com>
2023-03-17 10:01:27 -0400

    fix

--- lib/redirect.ts
+++ lib/redirect.ts
@@ -1,37 +1,23 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import languageDetector from './languageDetector';
 
 export const useRedirect = (to?: any) => {
 	const router = useRouter();
-	const [url, setUrl] = useState('');
 	to = to || router.asPath;
+
 	// language detection
-	//check if in localhost, if not just add the .html extension to the url when loading
 	useEffect(() => {
-		if (!url) {return}
 		const detectedLng = languageDetector.detect();
 		if (to.startsWith('/' + detectedLng) && router.route === '/404') {
 			// prevent endless loop
-			if (url === 'localhost:3000') {
-				router.replace('/' + detectedLng + router.route);
-			} else {
-				router.replace('/' + detectedLng + '.html' )
-			}
+			router.replace('/' + detectedLng + router.route);
 			return;
 		}
 
-		if (url === 'localhost:3000') {
-			router.replace('/' + detectedLng + to);
-		} else {
-			router.replace('/' + detectedLng + '.html' )
-		}
 		languageDetector.cache?.(detectedLng!);
-	}, [url])
-
-	useEffect(() => {
-		setUrl(window ? window.location.host : '');
-	}, []);
+		router.replace('/' + detectedLng + to);
+	});
 
 	return;
 };
